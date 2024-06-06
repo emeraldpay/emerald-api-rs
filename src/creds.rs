182c8692c4809a2c58050f9910a4b6eae853f812
Igor Artamonov <igor@artamonov.ru>
2024-06-06 13:52:28 +0100

    solution: jwt token to authenticate requests

--- src/creds.rs
+++ src/creds.rs
@@ -4,6 +4,7 @@ use tonic::service::Interceptor;
 #[derive(Debug, Clone)]
 pub enum Credentials {
     None,
+    Jwt(String),
 }
 
 impl Default for Credentials {
@@ -13,13 +14,30 @@ impl Default for Credentials {
 }
 
 impl Credentials {
+
+    ///
+    /// Do nothing
     pub fn unauthneticated() -> Self {
         Credentials::None
+    }
+
+    ///
+    /// Authenticate using a JWT token, i.e., by putting it in the `Authorization` header
+    pub fn jwt<S: ToString>(jwt: S) -> Self {
+        Credentials::Jwt(jwt.to_string())
     }
 }
 
 impl Interceptor for Credentials {
     fn call(&mut self, request: Request<()>) -> Result<Request<()>, Status> {
-        Ok(request)
+        match self {
+            Credentials::None => Ok(request),
+            Credentials::Jwt(s) => {
+                let (mut meta, ext, m) = request.into_parts();
+                let header_value  = format!("Bearer {}", s);
+                meta.insert("authorization", header_value.as_str().parse().unwrap());
+                Ok(Request::from_parts(meta, ext, m))
+            }
+        }
     }
 }
